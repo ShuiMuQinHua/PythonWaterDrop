c1b9f3985d635b7e64c0a97e24a48b547effcb92
can_pc <1835373375@qq.com>
2017-02-09 14:45:33 +0800

    函数练习

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+#所有参数（变量）都是按引用传递
 def printme(str):
 	"打印传入的字符串到标准显示设备上"
 	print(str)
@@ -43,12 +43,55 @@ total = sum( 10, 20 );
 print("函数外 : ", total)
 
 
+#python的所有参数都是按引用传递
+# 可写函数说明
+def changeme(mylist):
+	"修改传入的列表"
+	mylist.append([1, 2, 3, 4]);
+	print("函数内取值: ", mylist)
+	return
+
+# 调用changeme函数
+mylist = [10, 20, 30];
+changeme(mylist);
+print("函数外取值: ", mylist)
 
 
 
+# 加了星号（*）的变量名会存放所有未命名的变量参数。如果在函数调用时没有指定参数，它就是一个空元组。我们也可以不向函数传递未命名的变量。
+# 可写函数说明
+def printinfo(arg1, *vartuple):
+	"打印任何传入的参数"
+	print("输出: ")
+	print(arg1)
+	for var in vartuple:
+		print(var)
+	return;
+
+# 调用printinfo 函数
+printinfo(10);
+printinfo(70, 60, 50);
+
+
+#匿名函数
+# 可写函数说明
+sum = lambda arg1, arg2: arg1 + arg2;
+# 调用sum函数
+print("相加后的值为 : ", sum(10, 20))  #30
+print("相加后的值为 : ", sum(20, 20))  #40
 
 
 
+# return返回 练习
+# 可写函数说明
+def sum( arg1, arg2 ):
+   # 返回2个参数的和."
+   total = arg1 + arg2
+   print ("函数内 : ", total)
+   return total;
 
+# 调用sum函数
+total = sum( 10, 20 );
+print ("函数外 : ", total)
 
 
